a04498d1359b144eb1170ca8840b84dca86aff40
stephenbeese <stephenbeese1@live.co.uk>
2023-05-10 14:48:45 +0000

    Create admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import GameTable, Reservation, Testimonial
 
-# Register your models here.
+# admin.site.register(GameTable)
+# admin.site.register(Reservation)
+# admin.site.register(Testimonial)
+
+
+@admin.register(GameTable)
+class GameTableAdmin(admin.ModelAdmin):
+    list_display = ('game_id', 'game_type', 'available_from', 'available_to', 'price', 'status')
+    list_filter = ('game_type', 'status')
+    search_fields = ['game_type']
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('booking_id', 'user_id', 'name', 'date', 'start_time', 'end_time', 'total_price')
+    list_filter = ('date', 'start_time', 'end_time')
+    search_fields = ['name', 'date']
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_filter = ('created_on', 'approved')
+    list_display = ('user_id', 'name', 'comment', 'created_on', 'approved')
+    search_fields = ['name', 'comment']
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# SNOOKER = 0
+# EN_POOL = 1
+# AM_POOL = 2
+
+STATUS = ((0, "Working"), (1, "Maintenance"))
+GAME = ((0, "Snooker"), (1, "English Pool"), (2, "American Pool"))
+
+
+class GameTable(models.Model):
+    game_id = models.AutoField(primary_key=True)
+    game_type = models.IntegerField(choices=GAME)
+    available_from = models.TimeField()
+    available_to = models.TimeField()
+    price = models.IntegerField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-game_type']
+
+    def __str__(self):
+        if self.game_type == 0:
+            return f"Snooker table, ID: {self.game_id}, status: {self.status}"
+        elif self.game_type == 1:
+            return f"English Pool table, ID: {self.game_id}, status: {self.status}"
+        else:
+            return f"American table, ID: {self.game_id}, status: {self.status}"
+
+
+class Reservation(models.Model):
+    booking_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_bookings")
+    game_id = models.ForeignKey(GameTable(), on_delete=models.CASCADE, related_name="table_bookings")
+    name = models.CharField(max_length=40)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    total_price = models.IntegerField()
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return f"Booked by {self.name} for {self.date} at {self.start_time}"
+
+
+class Testimonial(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="testimonials")
+    name = models.CharField(max_length=40)
+    comment = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment: {self.comment} by {self.name}"
