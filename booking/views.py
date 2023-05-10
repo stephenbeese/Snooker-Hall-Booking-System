58c5006a10803571c8b9c1426c857b36f2ec7bfb
stephenbeese <stephenbeese1@live.co.uk>
2023-05-10 17:19:13 +0000

    Create view to render basic html and css

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Testimonial
 
-# Create your views here.
+
+class TestimonialList(generic.ListView):
+    model = Testimonial
+    queryset = Testimonial.objects.filter(approved=True).order_by('-created_on')
+    template_name = 'index.html'
