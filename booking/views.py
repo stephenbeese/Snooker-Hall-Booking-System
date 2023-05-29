aea08d5bc580eb9a766d1f3b5fd08a23d05634a4
stephenbeese <stephenbeese1@live.co.uk>
2023-05-29 21:06:20 +0000

    Add docstrings to views.py file

--- booking/views.py
+++ booking/views.py
@@ -1,21 +1,32 @@
 from datetime import datetime, date
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
+from django import template
 from .models import Testimonial, GameTable, Reservation
 from .forms import TestimonialForm, ReservationForm
-from django import template
 
 register = template.Library()
 
 
 class Home(generic.ListView):
+    """
+    View for the home page.
+
+    Displays a list of approved testimonials on the home page.
+    """
     model = Testimonial
     queryset = Testimonial.objects.filter(approved=True).order_by('-created_on')
     template_name = 'index.html'
 
 
 class TestimonialView(View):
+    """Allows users to submit testimonials through a form"""
     def get(self, request):
+        """
+        Handle GET request for submitting a testimonial.
+
+        Renders the testimonial submission form
+        """
         return render(
             request,
             "review.html",
@@ -26,6 +37,11 @@ class TestimonialView(View):
         )
 
     def post(self, request):
+        """
+        Handle POST request for submitting a testimonial.
+
+        Saves the submitted testimonial if it is valid.
+        """
         testimonial_form = TestimonialForm(request.POST)
 
         if testimonial_form.is_valid():
@@ -47,6 +63,11 @@ class TestimonialView(View):
 
 
 class GameTables(generic.ListView):
+    """
+    View for displaying game tables.
+
+    Lists all game tables in the database.
+    """
     model = GameTable
     template_name = 'tables.html'
     context_object_name = 'tables'
@@ -54,7 +75,17 @@ class GameTables(generic.ListView):
 
 
 class ReservationView(View):
+    """
+    View for making reservations.
+
+    Allows users to make reservations through a form.
+    """
     def get(self, request):
+        """
+        Handle GET request for making a reservation.
+
+        Renders the reservation form.
+        """
         form = ReservationForm()
         game_tables = GameTable.objects.all()
         context = {
@@ -64,6 +95,12 @@ class ReservationView(View):
         return render(request, 'reservation.html', context)
 
     def post(self, request):
+        """
+        Handle POST request for making a reservation.
+
+        Saves the reservation if it is valid and performs custom validation to
+        prevent conflicting reservations.
+        """
         form = ReservationForm(request.POST)
         game_tables = GameTable.objects.all()
         context = {
@@ -102,35 +139,34 @@ class ReservationView(View):
         return render(request, 'reservation.html', context)
 
 
-# class UserBookings(generic.ListView):
-#     model = Reservation
-#     template_name = 'bookings.html'
-#     context_object_name = 'bookings'
-#     ordering = '-date'
-
-#     def get_queryset(self):
-#         today = date.today()
-#         if self.request.user.is_authenticated:
-#             user_bookings = Reservation.objects.filter(user_id=self.request.user)
-#             past_bookings = user_bookings.filter(date__lt=today).order_by('-date')
-#             future_bookings = user_bookings.filter(date__gte=today).order_by('date')
-#             return list(past_bookings) + list(future_bookings)
-
-
 class UserBookings(generic.ListView):
+    """
+    View for displaying user bookings.
+
+    Lists the past and future bookings of the authenticated user.
+    """
     model = Reservation
     template_name = 'bookings.html'
-    context_object_name = 'past_bookings'  # Update context_object_name
+    context_object_name = 'past_bookings'
 
     def get_queryset(self):
+        """
+        Get the queryset for past bookings.
+
+        Returns a queryset of past bookings for the authenticated user.
+        """
         today = date.today()
         if self.request.user.is_authenticated:
             user_bookings = Reservation.objects.filter(user_id=self.request.user)
             past_bookings = user_bookings.filter(date__lt=today).order_by('-date')
-            future_bookings = user_bookings.filter(date__gte=today).order_by('date')
-            return past_bookings  # Return only the past bookings queryset
+            return past_bookings
 
     def get_context_data(self, **kwargs):
+        """
+        Get additional context data.
+
+        Adds the future bookings to the context data.
+        """
         context = super().get_context_data(**kwargs)
         today = date.today()
         if self.request.user.is_authenticated:
@@ -140,7 +176,17 @@ class UserBookings(generic.ListView):
 
 
 class EditBooking(View):
+    """
+    View for editing a booking.
+
+    Allows users to edit an existing booking.
+    """
     def get(self, request, booking_id):
+        """
+        Handle GET request for editing a booking.
+
+        Renders the booking edit form.
+        """
         booking = get_object_or_404(Reservation, booking_id=booking_id)
         form = ReservationForm(instance=booking)
         context = {
@@ -149,6 +195,11 @@ class EditBooking(View):
         return render(request, 'edit_booking.html', context)
 
     def post(self, request, booking_id):
+        """
+        Handle POST request for editing a booking.
+
+        Saves the edited booking if it is valid.
+        """
         booking = get_object_or_404(Reservation, booking_id=booking_id)
         if request.method == 'POST':
             form = ReservationForm(request.POST, instance=booking)
@@ -174,19 +225,44 @@ class EditBooking(View):
 
 
 class ConfirmDelete(View):
+    """
+    View for confirming booking deletion.
+
+    Displays a confirmation page before deleting a booking.
+    """
     def get(self, request, booking_id):
+        """
+        Handle GET request for confirming booking deletion.
+
+        Renders the confirmation page.
+        """
         booking = get_object_or_404(Reservation, booking_id=booking_id)
         context = {'booking': booking}
         return render(request, 'confirm_delete.html', context)
 
 
 class DeleteBooking(View):
+    """
+    View for deleting a booking.
+
+    Deletes a booking and redirects to the bookings page.
+    """
     def get(self, request, booking_id):
+        """
+        Handle GET request for deleting a booking.
+
+        Renders the confirmation page.
+        """
         booking = get_object_or_404(Reservation, booking_id=booking_id)
         context = {'booking': booking}
         return render(request, 'confirm_delete.html', context)
 
     def post(self, request, booking_id):
+        """
+        Handle POST request for deleting a booking.
+
+        Deletes the booking and redirects to the bookings page.
+        """
         booking = get_object_or_404(Reservation, booking_id=booking_id)
         booking.delete()
         return redirect('bookings')
